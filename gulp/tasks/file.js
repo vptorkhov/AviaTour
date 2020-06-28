14edf26bdfc764a6ca31511a8a2ea3e2d16f479b
Pavel <wol1414@gmail.com>
2020-06-29 01:40:02 +0300

    first commit

--- gulp/tasks/file.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (){
-
-	
-	$.gulp.task('file', function () {
- 
-		return 	$.gulp.src([$.sourse + '/libs/**']).pipe($.gulp.dest($.public + '/libs')).on('end',$.browserSync.reload);
-});
-
-}
