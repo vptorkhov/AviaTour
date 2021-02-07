ba3d83942405a9a39437d4fb68cf512e04c91406
Pavel <wol1414@gmail.com>
2021-02-07 14:34:21 +0300

    update

--- block.js
+++ block.js
@@ -26,9 +26,9 @@ const fileSources = {
 	scss: `
 	// start .{blockName}
 	.{blockName} \{
-		--sPT: ;
-		--sPB: ;
-		--sTPB: ;
+		// --sPT: ;
+		// --sPB: ;
+		// --sTPB: ;
 		 
 	@include media-breakpoint-up(xl) {}
 	@include media-breakpoint-up(lg) {}
